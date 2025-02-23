43be2d1e0f93fc75fc3f54e3f9aa09792db2b593
sjt-wtag <sumaya.jahan@welldev.io>
2025-02-23 09:13:20 +0600

    solve challenge using js map, arry and promise concept

--- challenge.mjs
+++ challenge.mjs
@@ -1,5 +1,29 @@
+const pendingRequests = new Map();
+
 export async function blockingGet(key) {
+  return new Promise((resolve) => {
+    if (pendingRequests.has(key)) pendingRequests.get(key).push(resolve);
+    else pendingRequests.set(key, [resolve]);
+
+    setTimeout(() => {
+      if (pendingRequests.has(key)) {
+        const index = pendingRequests.get(key).indexOf(resolve); //indexOf() returns first index of matched key data
+
+        if (index !== -1) {
+          pendingRequests.get(key).splice(index, 1);
+        }
+        resolve(null);
+      }
+    }, 40000);
+  });
 }
 
 export async function push(key, data) {
+  if (pendingRequests.has(key)) {
+    const resolveArray = pendingRequests.get(key);
+    const resolve = resolveArray.shift(); //shift() removes & returns first resolve
+
+    if (resolveArray.length === 0) pendingRequests.delete(key);
+    resolve(data);
+  }
 }
